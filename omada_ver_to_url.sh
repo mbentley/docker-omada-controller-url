2b7e173101ca26ddada4c63156624417644ac8ec
Matt Bentley <mbentley@mbentley.net>
2024-07-10 06:18:53 -0400

    Added new version (5.14.26.1) + legacy versions
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- omada_ver_to_url.sh
+++ omada_ver_to_url.sh
@@ -3,14 +3,50 @@
 set -e
 
 case "${1}" in
+  3.2.4)
+    OMADA_URL="https://static.tp-link.com/2019/201911/20191108/Omada_Controller_v3.2.4_linux_x64.tar.gz"
+    ;;
+  3.2.6)
+    OMADA_URL="https://static.tp-link.com/2020/202001/20200116/Omada_Controller_v3.2.6_linux_x64.tar.gz"
+    ;;
+  3.2.9)
+    OMADA_URL="https://static.tp-link.com/2020/202004/20200409/Omada_Controller_v3.2.9_linux_x64.tar.gz"
+    ;;
+  3.2.10)
+    OMADA_URL="https://static.tp-link.com/2020/202004/20200420/Omada_Controller_v3.2.10_linux_x64.tar.gz"
+    ;;
+  3.2.14)
+    OMADA_URL="https://static.tp-link.com/2020/202012/20201225/Omada_Controller_v3.2.14_linux_x64.tar.gz"
+    ;;
+  3.2.16)
+    OMADA_URL="https://static.tp-link.com/upload/software/2022/202201/20220125/Omada_Controller_v3.2.16_linux_x64.tar.gz"
+    ;;
+  3.2.17)
+    OMADA_URL="https://static.tp-link.com/upload/software/2022/202208/20220818/Omada_Controller_v3.2.17_Linux_x64.tar.gz"
+    ;;
   4.1|4.1.5)
     OMADA_URL="https://static.tp-link.com/2020/202007/20200714/Omada_SDN_Controller_v4.1.5_linux_x64.tar.gz"
+    ;;
+  4.2.4)
+    OMADA_URL="https://static.tp-link.com/2020/202011/20201110/Omada_SDN_Controller_v4.2.4_linux_x64.tar.gz"
+    ;;
+  4.2.8)
+    OMADA_URL="https://static.tp-link.com/2020/202012/20201211/Omada_SDN_Controller_v4.2.8_linux_x64.tar.gz"
     ;;
   4.2|4.2.11)
     OMADA_URL="https://static.tp-link.com/2021/202102/20210209/Omada_SDN_Controller_v4.2.11_linux_x64.tar.gz"
     ;;
   4.3|4.3.5)
     OMADA_URL="https://static.tp-link.com/2021/202105/20210507/Omada_SDN_Controller_v4.3.5_linux_x64.tar.gz"
+    ;;
+  4.4.3)
+    OMADA_URL="https://static.tp-link.com/software/2021/202107/20210701/Omada_SDN_Controller_v4.4.3_linux_x64.tar.gz"
+    ;;
+  4.4.4)
+    OMADA_URL="https://static.tp-link.com/upload/software/2021/202108/20210813/Omada_SDN_Controller_v4.4.4_linux_x64.tar.gz"
+    ;;
+  4.4.6)
+    OMADA_URL="https://static.tp-link.com/upload/software/2021/202110/20211011/Omada_SDN_Controller_v4.4.6_Linux_x64.tar.gz"
     ;;
   4.4|4.4.8)
     OMADA_URL="https://static.tp-link.com/upload/software/2021/202112/20211217/Omada_SDN_Controller_v4.4.8_linux_x64.tar.gz"
@@ -39,6 +75,9 @@ case "${1}" in
   5.8|5.8.4)
     OMADA_URL="https://static.tp-link.com/upload/software/2023/202301/20230130/Omada_SDN_Controller_v5.8.4_Linux_x64.tar.gz"
     ;;
+  5.9.9)
+    OMADA_URL="https://static.tp-link.com/upload/software/2023/202302/20230227/Omada_SDN_Controller_v5.9.9_Linux_x64.tar.gz"
+    ;;
   5.9|5.9.31)
     OMADA_URL="https://static.tp-link.com/upload/software/2023/202303/20230321/Omada_SDN_Controller_v5.9.31_Linux_x64.tar.gz"
     ;;
@@ -53,6 +92,9 @@ case "${1}" in
     ;;
   5.13|5.13.30.8)
     OMADA_URL="https://static.tp-link.com/upload/software/2024/202402/20240227/Omada_SDN_Controller_v5.13.30.8_linux_x64.tar.gz"
+    ;;
+  5.14|5.14.26.1)
+    OMADA_URL="https://static.tp-link.com/upload/software/2024/202407/20240710/Omada_SDN_Controller_v5.14.26.1_linux_x64.tar.gz"
     ;;
   beta-5.13.30.4)
     OMADA_URL="https://static.tp-link.com/upload/beta/2024/202401/20240109/Omada_SDN_Controller_v5.13.30.4_Linux_x64.tar.gz(Beta).gz"
