2488a0cf4118eaa019e9775fbd0f4f4017abf07d
Matt Bentley <mbentley@mbentley.net>
2025-01-24 06:04:13 -0500

    Added latest beta
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- omada_ver_to_url.sh
+++ omada_ver_to_url.sh
@@ -135,8 +135,11 @@ case "${1}" in
   beta-5.15.8.1)
     OMADA_URL="https://static.tp-link.com/upload/beta/2024/202412/20241224/Omada_SDN_Controller_v5.15.8.1_pre-release_linux_x64_tar.gz.zip"
     ;;
-  beta|beta-5.15.8.2)
+  beta-5.15.8.2)
     OMADA_URL="https://static.tp-link.com/upload/beta/2025/202501/20250106/Omada_SDN_Controller_v5.15.8.2_pre-release_linux_x64_tar.gz.zip.zip"
+    ;;
+  beta|beta-5.15.20.10)
+    OMADA_URL="https://static.tp-link.com/upload/beta/2025/202501/20250124/Omada_SDN_Controller_v5.15.20.10_pre-release_linux_x64_tar.gz.zip.zip"
     ;;
   *)
     exit 1
