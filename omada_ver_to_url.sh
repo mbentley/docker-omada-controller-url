982c6a864ebc6cac73ec0782e1e13ac24f4ec30a
Matt Bentley <mbentley@mbentley.net>
2026-02-12 07:05:20 -0500

    Added beta 6.2.0.12
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- omada_ver_to_url.sh
+++ omada_ver_to_url.sh
@@ -201,8 +201,11 @@ case "${1}" in
   beta-6.1.0.18)
     OMADA_URL="https://static.tp-link.com/upload/beta/2026/202601/20260109/Omada_Network_Application_v6.1.0.18_linux_x64_20260109092422.tar.gz.zip"
     ;;
-  beta|beta-6.1.0.19)
+  beta-6.1.0.19)
     OMADA_URL="https://static.tp-link.com/upload/beta/2026/202601/20260119/Omada_Network_Application_v6.1.0.19_linux_x64_20260117100056.tar.gz.zip"
+    ;;
+  beta|beta-6.2.0.12)
+    OMADA_URL="https://static.tp-link.com/upload/beta/2026/202602/20260211/Omada_Network_Application_v6.2.0.12_linux_x64_20260210143425.tar.gz.zip"
     ;;
   *)
     exit 1
